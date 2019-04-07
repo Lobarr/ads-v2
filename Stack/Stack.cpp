8c9e1d1ead2f9d03fbe99a690959fd5b47175ca6
Lobarr <jesulobaegunjobi@hotmail.com>
2019-04-07 14:27:08 -0700

    refactored stack to use DLinkedList

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -4,26 +4,19 @@ template <typename T>
 Stack<T>::Stack() {}
 
 template <typename T>
-Stack<T>::Stack(const Stack<T>& cp) {
-  for(int i = 0; i < cp.getStack().size(); i++) {
-    stack.push_back(cp.getStack().at(i));
-  }
-}
-
-template <typename T>
 void Stack<T>::push(const T& input) {
-  stack.push_back(input);
+  stack.addFront(input);
 }
 
 template <typename T>
 void Stack<T>::pop() {
-  stack.pop_back();
+  stack.removeFront();
 }
 
 template <typename T>
 T Stack<T>::top() {
   if (stack.empty()) throw "Stack Empty";
-  return stack.at(0);;
+  return stack.at(stack.getCount()-1)->data;
 }
 
 template <typename T>
@@ -33,19 +26,10 @@ bool Stack<T>::empty() {
 
 template <typename T>
 int Stack<T>::size() const {
-  return stack.size();
-}
-
-template <typename T>
-std::vector<T> Stack<T>::getStack() const {
-  return stack;
+  return stack.getCount();
 }
 
 template <typename T>
 void Stack<T>::print() const {
-  for (int i = 0; i < stack.size(); i++) {
-    std::cout << "Index: " << i << std::endl;
-    std::cout << "Value: " << stack.at(i) << std::endl;
-  }
-  std::cout << "-- Done --" << std::endl;
+  stack.printDes();
 }
--- Stack/Stack.h
+++ Stack/Stack.h
@@ -1,26 +1,24 @@
 #ifndef STACK_H
 #define STACK_H
 
-#include <vector>
+// #include <vector>
+#include "../LinkedLists/DLinkedList/DLinkedList.cpp"
 #include <iostream>
 
 template <typename T>
 class Stack {
   public:
     Stack();
-    Stack(const Stack& cp);
 
     void push(const T& input);
     void pop();
     T top();
     bool empty();
     int size() const;
-
-    std::vector<T> getStack() const;
     void print() const;
   
   private:
-    std::vector<T> stack;
+    DLinkedList<T> stack;
 };
 
 #endif
