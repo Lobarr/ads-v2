9fabb1f10330bffbe666a9a488fdfdcb04f4c8a7
Lobarr <jesulobaegunjobi@hotmail.com>
2019-04-27 11:59:00 -0700

    progress on binary tree

--- Trees/BinaryTree/BinaryTree.cpp
+++ Trees/BinaryTree/BinaryTree.cpp
@@ -1,7 +1,8 @@
 #include "./BinaryTree.h"
 
 template <typename T>
-BNode<T>::BNode() {
+BNode<T>::BNode(T& d) {
+  data = d;
   left = right = parent = NULL;
 }
 
@@ -21,6 +22,11 @@ const T BNode<T>::getData() const {
 template <typename T>
 const BNode<T>* BNode<T>::getParent() const {
   return parent;
+}
+
+template <typename T>
+void BNode<T>::setParent(BNode<T>& n) {
+  parent = n;
 }
 
 template <typename T>
@@ -51,4 +57,69 @@ const bool BNode<T>::isRoot() const {
 template <typename T>
 const bool BNode<T>::isLeaf() const {
   return left == NULL && right == NULL && parent != NULL;
+}
+
+template <typename T>
+BinaryTree<T>::BinaryTree() {
+  root = NULL;
+  size = 0;
+}
+
+template <typename T>
+BinaryTree<T>::BinaryTree(BNode<T>& n) {
+  root = n;
+  size = 1;
+}
+
+template <typename T>
+const bool BinaryTree<T>::empty() const {
+  return root == NULL;
+}
+
+template <typename T>
+const int BinaryTree<T>::getSize() const {
+  return size;
+}
+
+template <typename T>
+const BNode<T>* BinaryTree<T>::getRoot() const {
+  return root;
+}
+
+template <typename T>
+void BinaryTree<T>::addNode(BNode<T>& n) {
+  if (root == NULL) {
+    root = n;
+  } else {
+    BNode<T>* curNode = root;
+    while(curNode != NULL) {
+      n.setParent(curNode);
+      curNode = (n->getData() > curNode->getData()) ? curNode->getRight() : curNode->getLeft();
+    }
+    BNode<T>* parent = n.getParent();
+    if(n.getData() > parent.getData()) {
+      parent.setRight(n);
+    } else {
+      parent.setLeft(n);
+    }
+  }
+  size++;
+}
+
+template <typename T>
+void BinaryTree<T>::removeNode(BNode<T>* cur, BNode<T>& n) {
+
+}
+
+template <typename T>
+const BNode<T>* BinaryTree<T>::search(T& d) const {
+  BNode<T>* curNode = root;
+  while(curNode != NULL) {
+    if (curNode->getData() == d) {
+      return curNode;
+    } else {
+      curNode = (curNode->getData() < d) ? curNode->getRight() : curNode->getLeft();
+    }
+  }
+  return NULL;
 }
--- Trees/BinaryTree/BinaryTree.h
+++ Trees/BinaryTree/BinaryTree.h
@@ -4,10 +4,11 @@
 template <typename T>
 class BNode {
   public:
-    BNode();
+    BNode(T& d);
     BNode(T& d, BNode<T>* l, BNode<T>* r, BNode<T>* p);
     const T getData() const;
     const BNode<T>* getParent() const;
+    void setParent(BNode<T>& n);
     const BNode<T>* getLeft() const;
     void setLeft(BNode<T>& n);
     const BNode<T>* getRight() const;
@@ -27,9 +28,16 @@ class BinaryTree {
     BinaryTree();
     BinaryTree(BNode<T>& n);
     const bool empty() const;
+    const int getSize() const;
     const BNode<T>* getRoot() const;
+    void addNode(BNode<T>& n);
+    void removeNode(BNode<T>* cur, BNode<T>& n);
+    const BNode<T>* search(T& d) const;
+    void preOrderPrint() const;
+    void postOrderPrint() const;
   private:
     BNode<T>* root;
+    int size;
 };
 
 #endif 
